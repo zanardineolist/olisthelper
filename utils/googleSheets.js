25156fe547ce45158cdd9f40e29b7797b319f509
zanardineolist <rafael.rocha@tiny.com.br>
2024-10-18 19:26:30 -0300

    new

--- utils/googleSheets.js
+++ utils/googleSheets.js
@@ -1,20 +1,46 @@
-import { google } from 'googleapis';
+import jwt from 'jsonwebtoken';
 
 export async function appendToSheet(data) {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: 'path/to/credentials.json',
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
-
-  const sheets = google.sheets({ version: 'v4', auth });
+  const privateKey = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n');
+  const serviceAccountEmail = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
   const spreadsheetId = process.env.SPREADSHEET_ID;
 
-  await sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range: 'AnalistaEscolhido!A:C',
-    valueInputOption: 'USER_ENTERED',
-    resource: {
-      values: [data],
+  // Gerar o token JWT para autenticar na API do Google Sheets
+  const token = jwt.sign(
+    {
+      iss: serviceAccountEmail,
+      scope: 'https://www.googleapis.com/auth/spreadsheets',
+      aud: 'https://oauth2.googleapis.com/token',
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+      iat: Math.floor(Date.now() / 1000),
     },
+    privateKey,
+    { algorithm: 'RS256' }
+  );
+
+  const tokenResponse = await fetch('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=${token}`,
   });
+
+  const { access_token } = await tokenResponse.json();
+
+  const response = await fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/AnalistaEscolhido!A:C:append?valueInputOption=USER_ENTERED`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        values: [data],
+      }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error('Erro ao enviar dados para o Google Sheets');
+  }
 }
