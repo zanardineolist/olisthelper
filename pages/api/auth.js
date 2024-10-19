070a1dd907ac35b6d42a0ac0eecc5b9aafaf8fcd
zanardineolist <rafael.rocha@tiny.com.br>
2024-10-19 00:03:58 -0300

    new

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,6 +1,18 @@
 import { google } from 'googleapis';
 import bcrypt from 'bcryptjs';
 
+// Definindo cada parte das credenciais do Google separadamente
+const GOOGLE_CREDENTIALS_TYPE = process.env.GOOGLE_CREDENTIALS_TYPE;
+const GOOGLE_PROJECT_ID = process.env.GOOGLE_PROJECT_ID;
+const GOOGLE_PRIVATE_KEY_ID = process.env.GOOGLE_PRIVATE_KEY_ID;
+const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n');
+const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_AUTH_URI = process.env.GOOGLE_AUTH_URI;
+const GOOGLE_TOKEN_URI = process.env.GOOGLE_TOKEN_URI;
+const GOOGLE_AUTH_PROVIDER_X509_CERT_URL = process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL;
+const GOOGLE_CLIENT_X509_CERT_URL = process.env.GOOGLE_CLIENT_X509_CERT_URL;
+
 const sheets = google.sheets('v4');
 
 export default async function handler(req, res) {
@@ -15,11 +27,21 @@ export default async function handler(req, res) {
 
     try {
       // Autenticação no Google Sheets
-      console.log('Google Credentials:', process.env.GOOGLE_CREDENTIALS);
-        const auth = new google.auth.GoogleAuth({
-          credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
-          scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-        });
+      const auth = new google.auth.GoogleAuth({
+        credentials: {
+          type: GOOGLE_CREDENTIALS_TYPE,
+          project_id: GOOGLE_PROJECT_ID,
+          private_key_id: GOOGLE_PRIVATE_KEY_ID,
+          private_key: GOOGLE_PRIVATE_KEY,
+          client_email: GOOGLE_CLIENT_EMAIL,
+          client_id: GOOGLE_CLIENT_ID,
+          auth_uri: GOOGLE_AUTH_URI,
+          token_uri: GOOGLE_TOKEN_URI,
+          auth_provider_x509_cert_url: GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+          client_x509_cert_url: GOOGLE_CLIENT_X509_CERT_URL,
+        },
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+      });
 
       const client = await auth.getClient();
       google.options({ auth: client });
