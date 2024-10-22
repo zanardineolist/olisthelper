7209e2fb4544ca4a8633a8e90880f83899fe746a
zanardineolist <rafael.rocha@tiny.com.br>
2024-10-21 23:17:51 -0300

    new

--- middleware.js
+++ middleware.js
@@ -1,16 +1,21 @@
-import { getToken } from 'next-auth/jwt';
-import { NextResponse } from 'next/server';
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  // Rotas protegidas: /registrar, /my e /dashboard-analyst
   const protectedPaths = ['/registrar', '/my', '/dashboard-analyst'];
   const pathIsProtected = protectedPaths.some((path) =>
     req.nextUrl.pathname.startsWith(path)
   );
 
-  if (pathIsProtected && (!token || token.role !== 'analyst')) {
+  if (pathIsProtected && !token) {
+    if (req.nextUrl.pathname !== '/') {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+  }
+
+  if (req.nextUrl.pathname.startsWith('/dashboard-analyst') && token?.role !== 'analyst') {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
