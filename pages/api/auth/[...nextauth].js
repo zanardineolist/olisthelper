13048de7ede50452fd0777caa568cda141190525
zanardineolist <rafael.rocha@tiny.com.br>
2024-10-23 20:29:13 -0300

    Ajuste no registro de novos usuários

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { google } from 'googleapis';
-import { addUserToSheet } from '../../../utils/googleSheets'; // Corrigido o caminho da importação para a pasta utils
+import { addUserToSheet, getUserFromSheet } from '../../../utils/googleSheets'; // Importar as funções para lidar com a planilha
 
 async function getUserDetails(email) {
   const auth = new google.auth.JWT(
@@ -46,9 +46,19 @@ export default NextAuth({
       const cleanAuthorizedDomains = authorizedDomains.map(domain => domain.trim().toLowerCase().replace(/^@/, ''));
       const userDomain = user.email.split("@")[1].toLowerCase();
 
+      // Verificar se o domínio do usuário é autorizado
       if (cleanAuthorizedDomains.length > 0 && !cleanAuthorizedDomains.includes(userDomain)) {
         console.log('Usuário não autorizado - domínio não permitido:', userDomain);
         return false;
+      }
+
+      // Verificar se o usuário já está registrado na planilha
+      let userDetails = await getUserFromSheet(user.email);
+
+      if (!userDetails) {
+        console.log('Usuário não encontrado na planilha. Registrando novo usuário:', user.email);
+        // Registrar usuário automaticamente na planilha
+        await addUserToSheet({ name: user.name, email: user.email });
       }
 
       return true;
@@ -63,17 +73,12 @@ export default NextAuth({
     async jwt({ token, user }) {
       if (user) {
         // Obter detalhes do usuário da planilha
-        let userDetails = await getUserDetails(user.email);
+        let userDetails = await getUserFromSheet(user.email);
 
-        // Se o usuário não existir na planilha, registrá-lo
-        if (!userDetails.id) {
-          console.log('Usuário não encontrado na planilha. Registrando novo usuário:', user.email);
-          await addUserToSheet({ name: user.name, email: user.email });
-          userDetails = await getUserDetails(user.email);
+        if (userDetails) {
+          token.id = userDetails[0]; // Armazena o ID de 4 dígitos da planilha
+          token.role = userDetails[3]; // Armazena o papel do usuário (e.g., 'user')
         }
-
-        token.id = userDetails.id; // Armazena o ID de 4 dígitos da planilha
-        token.role = userDetails.role; // Armazena o papel do usuário
       }
       return token;
     },
