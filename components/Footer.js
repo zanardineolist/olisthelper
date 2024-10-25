621dae3d107b04e50dc8bff0cebecd0dad103cf6
zanardineolist <rafael.rocha@tiny.com.br>
2024-10-25 19:57:56 -0300

    Open AI logo footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,11 @@
 // components/Footer.js
 import styles from '../styles/Footer.module.css';
-import openAILogo from '../../public/images/logos/OpenAI_logo.png';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.logoContainer}>
-        <img src={openAILogo} alt="OpenAI Logo" className={styles.logo} />
+        <img src="/images/logos/OpenAI_logo.png" alt="OpenAI Logo" className={styles.logo} />
       </div>
       <p>© Desenvolvido por Rafael Zanardine e Lucas Wenglarek</p>
     </footer>
