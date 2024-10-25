92f64a907b06c652760f3c62a1f821a30d93cdcc
zanardineolist <rafael.rocha@tiny.com.br>
2024-10-25 18:36:10 -0300

    Coloração nos indicadores

--- pages/my.js
+++ pages/my.js
@@ -87,7 +87,7 @@ export default function MyPage({ user }) {
   const formattedPercentage = Math.abs(percentageChange).toFixed(1);
 
   const getIndicatorColor = (value, threshold, isGreaterBetter = true) => {
-    return (isGreaterBetter ? value >= threshold : value <= threshold) ? '#00FF00' : '#FF0000';
+    return (isGreaterBetter ? value >= threshold : value <= threshold) ? '#779E3D' : '#E64E36';
   };
 
   return (
@@ -211,9 +211,13 @@ export default function MyPage({ user }) {
               <h2>Indicadores Chamados</h2>
               <p className={styles.lastUpdated}>Atualizado até: {performanceData?.atualizadoAte || "Data não disponível"}</p>
               <div className={styles.performanceInfo}>
-                <div className={styles.performanceItem} style={{ backgroundColor: getIndicatorColor(performanceData.chamados.totalChamados, 25) }}>
+              <div className={styles.performanceItem} style={{ backgroundColor: getIndicatorColor(performanceData.chamados.totalChamados, 25) }}>
                   <span>Total Chamados:</span>
                   <span>{performanceData.chamados.totalChamados}</span>
+                </div>
+                <div className={styles.performanceItem}>
+                  <span>Média/Dia:</span>
+                  <span>{performanceData.chamados.mediaPorDia}</span>
                 </div>
                 <div className={styles.performanceItem} style={{ backgroundColor: getIndicatorColor(performanceData.chamados.tma, 30, false) }}>
                   <span>TMA:</span>
@@ -231,6 +235,14 @@ export default function MyPage({ user }) {
               <h2>Indicadores Telefone</h2>
               <p className={styles.lastUpdated}>Atualizado até: {performanceData?.atualizadoAte || "Data não disponível"}</p>
               <div className={styles.performanceInfo}>
+                <div className={styles.performanceItem}>
+                  <span>Total Telefone:</span>
+                  <span>{performanceData.telefone.totalTelefone}</span>
+                </div>
+                <div className={styles.performanceItem}>
+                  <span>Média/Dia:</span>
+                  <span>{performanceData.telefone.mediaPorDia}</span>
+                </div>
                 <div className={styles.performanceItem} style={{ backgroundColor: getIndicatorColor(performanceData.telefone.tma, 15, false) }}>
                   <span>TMA:</span>
                   <span>{performanceData.telefone.tma}</span>
@@ -238,6 +250,10 @@ export default function MyPage({ user }) {
                 <div className={styles.performanceItem} style={{ backgroundColor: getIndicatorColor(performanceData.telefone.csat, 3.7) }}>
                   <span>CSAT:</span>
                   <span>{performanceData.telefone.csat}</span>
+                </div>
+                <div className={styles.performanceItem}>
+                  <span>Perdidas:</span>
+                  <span>{performanceData.telefone.perdidas}</span>
                 </div>
               </div>
             </div>
@@ -247,6 +263,14 @@ export default function MyPage({ user }) {
               <h2>Indicadores Chat</h2>
               <p className={styles.lastUpdated}>Atualizado até: {performanceData?.atualizadoAte || "Data não disponível"}</p>
               <div className={styles.performanceInfo}>
+                <div className={styles.performanceItem}>
+                  <span>Total Chats:</span>
+                  <span>{performanceData.chat.totalChats}</span>
+                </div>
+                <div className={styles.performanceItem}>
+                  <span>Média/Dia:</span>
+                  <span>{performanceData.chat.mediaPorDia}</span>
+                </div>
                 <div className={styles.performanceItem} style={{ backgroundColor: getIndicatorColor(performanceData.chat.tma, 20, false) }}>
                   <span>TMA:</span>
                   <span>{performanceData.chat.tma}</span>
