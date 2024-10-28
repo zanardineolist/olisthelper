6777c4a1063d836bf0abca6f88746cf9d80beb23
zanardineolist <rafael.rocha@tiny.com.br>
2024-10-27 23:46:41 -0300

    Super Dashboard

--- pages/api/get-super-dashboard-data.js
+++ pages/api/get-super-dashboard-data.js
@@ -1,7 +1,7 @@
 // pages/api/get-super-dashboard-data.js
-import { getUserPerformance } from '../../utils/get-user-performance';
-import { getUserHelpRequests } from '../../utils/get-user-help-requests';
-import { getUserCategoryRanking } from '../../utils/get-user-category-ranking';
+import { getUserPerformance } from './get-user-performance';
+import { getUserHelpRequests } from './get-user-help-requests';
+import { getUserCategoryRanking } from './get-user-category-ranking';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
