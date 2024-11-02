15c1a4b0a951eac7910e3527a2f90dbcc74a7797
zanardineolist <rafael.rocha@tiny.com.br>
2024-11-02 18:39:45 -0300

    integração com o firebase

--- utils/firebase/firebaseLogging.js
+++ utils/firebase/firebaseLogging.js
@@ -4,6 +4,9 @@ import { db } from "./firebaseConfig";
 
 export async function logAction(userId, userName, userRole, action, target, previousData, updatedData) {
   try {
+    console.log("Tentando registrar log no Firestore com os seguintes dados:");
+    console.log({ userId, userName, userRole, action, target, previousData, updatedData });
+
     const docRef = await addDoc(collection(db, "logs"), {
       userId,
       userName,
@@ -14,7 +17,8 @@ export async function logAction(userId, userName, userRole, action, target, prev
       updatedData,
       timestamp: serverTimestamp(),
     });
-    console.log("Log registrado com ID: ", docRef.id);
+
+    console.log("Log registrado com sucesso com ID:", docRef.id);
   } catch (error) {
     console.error("Erro ao registrar log no Firebase:", error);
   }
