ae9497a766cf7d9c20400537d90bf15662f4a237
zanardineolist <rafael.rocha@tiny.com.br>
2024-11-04 22:40:00 -0300

    funcionalidade de notificações

--- utils/firebase/firebaseNotifications.js
+++ utils/firebase/firebaseNotifications.js
@@ -6,22 +6,19 @@ import { db } from "./firebaseConfig";
 export async function addNotification(userId, title, message) {
   try {
     if (!userId || !title || !message) {
-      console.error("Dados faltando ao tentar adicionar notificação:", { userId, title, message });
       throw new Error("Dados insuficientes para adicionar notificação.");
     }
 
     const notificationsCollection = collection(db, "notifications");
-    const docRef = await addDoc(notificationsCollection, {
+    await addDoc(notificationsCollection, {
       userId,
       title,
       message,
       read: false,
-      timestamp: new Date().getTime(), // Salva como milissegundos
+      timestamp: new Date().getTime(),
     });
 
-    console.log(`Notificação adicionada com sucesso com ID: ${docRef.id}`);
   } catch (error) {
-    console.error("Erro ao adicionar notificação no Firebase:", error);
     throw new Error("Erro ao adicionar notificação no Firebase.");
   }
 }
@@ -30,7 +27,6 @@ export async function addNotification(userId, title, message) {
 export async function getUserNotifications(userId) {
   try {
     if (!userId) {
-      console.error("ID do usuário faltando ao tentar buscar notificações.");
       return [];
     }
 
@@ -40,7 +36,6 @@ export async function getUserNotifications(userId) {
 
     return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
   } catch (error) {
-    console.error("Erro ao buscar notificações no Firebase:", error);
     return [];
   }
 }
@@ -49,15 +44,13 @@ export async function getUserNotifications(userId) {
 export async function markNotificationAsRead(notificationId) {
   try {
     if (!notificationId) {
-      console.error("ID da notificação faltando ao tentar marcar como lida.");
       return;
     }
 
     const notificationDoc = doc(db, "notifications", notificationId);
     await updateDoc(notificationDoc, { read: true });
 
-    console.log(`Notificação com ID ${notificationId} marcada como lida.`);
   } catch (error) {
-    console.error("Erro ao marcar notificação como lida no Firebase:", error);
+    throw new Error("Erro ao marcar notificação como lida no Firebase.");
   }
 }
