de4da6eebd1a470ea2ebf3d6d17039594617309a
zanardineolist <rafael.rocha@tiny.com.br>
2024-11-19 20:37:44 -0300

    otimizações

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-import { getUserFromSheet } from "../utils/googleSheets";
+import { getUserFromSheet } from "./utils/googleSheets";
 
 export async function middleware(req) {
   let token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
