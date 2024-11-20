72f11819cc4398ce900d895622cabdc282218d72
zanardineolist <rafael.rocha@tiny.com.br>
2024-11-20 10:57:17 -0300

    melhorias no cacheamento e otimização do get-user e get-analyst-leaderboard

--- utils/edgeConfig.js
+++ utils/edgeConfig.js
@@ -1,4 +1,5 @@
 // utils/edgeConfig.js
+
 let edgeConfig = null;
 
 if (typeof window === 'undefined') {
@@ -9,9 +10,10 @@ if (typeof window === 'undefined') {
   }
 }
 
-async function getEdgeConfig(key) {
-  if (!edgeConfig) {
-    throw new Error("Edge Config não está disponível no ambiente atual.");
+export async function getEdgeConfig(key) {
+  if (!edgeConfig || typeof edgeConfig.get !== 'function') {
+    console.error("Edge Config não está disponível ou o método 'get' não existe.");
+    return null;
   }
 
   try {
@@ -22,15 +24,29 @@ async function getEdgeConfig(key) {
   }
 }
 
-async function setEdgeConfig(key, value, options) {
-  if (!edgeConfig) {
-    throw new Error("Edge Config não está disponível no ambiente atual.");
+export async function setEdgeConfig(key, value, options) {
+  if (!edgeConfig || typeof edgeConfig.set !== 'function') {
+    console.error("Edge Config não está disponível ou o método 'set' não existe.");
+    return;
   }
 
   try {
     await edgeConfig.set(key, value, options);
   } catch (error) {
     console.error(`Erro ao definir Edge Config para a chave ${key}:`, error);
+  }
+}
+
+export async function deleteEdgeConfig(key) {
+  if (!edgeConfig || typeof edgeConfig.delete !== 'function') {
+    console.error("Edge Config não está disponível ou o método 'delete' não existe.");
+    return;
+  }
+
+  try {
+    await edgeConfig.delete(key);
+  } catch (error) {
+    console.error(`Erro ao excluir Edge Config para a chave ${key}:`, error);
   }
 }
 
