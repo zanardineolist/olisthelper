c76593c4d91f7b6261cc8864d5769eb59b5e70ec
zanardineolist <rafael.rocha@tiny.com.br>
2024-11-23 10:10:09 -0300

    navbar update

--- components/Layout.js
+++ components/Layout.js
@@ -3,8 +3,10 @@ import Navbar from './Navbar';
 import styles from '../styles/Layout.module.css';
 
 export default function Layout({ children }) {
+  const hasBanner = true;
+
   return (
-    <div className={styles.layout}>
+    <div className={`${styles.layout} ${hasBanner ? styles.withBanner : ''}`}>
       <Navbar />
       <main className={styles.main}>
         {children}
