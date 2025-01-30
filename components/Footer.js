b4dae794135da67059381d777d03540da1cd6b57
zanardineolist <rafael.rocha@tiny.com.br>
2025-01-30 01:54:11 -0300

    Removendo o logo do gpt

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,6 @@ import styles from '../styles/Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.logoContainer}>
-        <img src="/images/logos/OpenAI_logo.png" alt="OpenAI Logo" className={styles.logo} />
-      </div>
       <p>© Desenvolvido por Rafael Zanardine e Lucas Wenglarek</p>
     </footer>
   );
