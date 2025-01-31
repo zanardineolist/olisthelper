8bdc7e528d4027f5461f638213860b529b3ef6db
zanardineolist <rafael.rocha@tiny.com.br>
2025-01-31 01:29:42 -0300

    nova pagina de ferramentas - compartilhar respostas

--- components/SharedMessages.js
+++ components/SharedMessages.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import { Tab, Tabs } from '@mui/material';
-import { FaHeart, FaRegHeart, FaCopy, FaEdit, FaTrash, FaPlus, FaMagic, FaUser, FaClock } from 'react-icons/fa';
+import { FaHeart, FaRegHeart, FaCopy, FaEdit, FaTrash, FaPlus, FaMagic, FaUser, FaClock, FaGlobe } from 'react-icons/fa';
 import Swal from 'sweetalert2';
 import styles from '../styles/SharedMessages.module.css';
 
