50c2e465f5e67ef1aa84c61c72bc8c04250018d9
ozanardine <ozanardine@gmail.com>
2025-02-27 17:02:35 -0300

    Ajustes

--- components/shared-messages/MessageList.js
+++ components/shared-messages/MessageList.js
@@ -5,7 +5,7 @@ import { useMessageContext } from './MessageContext';
 import MessageCard from './MessageCard';
 import MessageRow from './MessageRow';
 import Pagination from '../ui/Pagination';
-import styles from '../../styles/shared-messages/index.module.css';
+import styles from '../../styles/shared-messages/Layout.module.css';
 
 const MessageList = () => {
   const { 
