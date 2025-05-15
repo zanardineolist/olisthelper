de3b1028956af679c0087c7a4feabf72ba23cdb2
ozanardine <ozanardine@gmail.com>
2025-05-14 21:48:49 -0300

    Ajustes 7.0

--- components/ErrosComuns.js
+++ components/ErrosComuns.js
@@ -850,6 +850,7 @@ export default function ErrosComuns({ user }) {
             marginBottom: '16px', 
             backgroundColor: 'rgba(25, 118, 210, 0.08)', 
             border: '1px solid rgba(25, 118, 210, 0.2)',
+            color: 'rgb(226, 226, 226)',
             borderRadius: '8px',
             display: 'flex',
             alignItems: 'center',
